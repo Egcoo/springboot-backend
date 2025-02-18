7591f86828ba32e5b73964268c64619c74156829
Egcoo <fsjmissyou@gmail.com>
2025-02-18 20:26:51 +0800

    update  functions

---
+++ src/main/java/com/quiz/config/RedissonConfig.java
@@ -0,0 +1,42 @@
+package com.quiz.config;
+
+import lombok.Data;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Egcoo
+ * @date 2025/2/18 - 15:29
+ */
+
+/**
+ * Redisson 配置
+ * 参考官方文档：创建一个redis的客户端，用于和redis交互
+ */
+@Configuration
+@ConfigurationProperties(prefix = "spring.redis")
+@Data
+public class RedissonConfig {
+
+    private String host;
+
+    private Integer port;
+
+    private Integer database;
+
+    private String password;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://" + host + ":" + port)
+                .setDatabase(database)
+                .setPassword(password);
+        return Redisson.create(config);
+    }
+}
